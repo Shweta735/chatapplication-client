22fee1b721011ee7dd456ea4b28180c3b4f50a54
shweta735 <agarwalshweta735@gmail.com>
2021-08-03 13:35:44 +0530

    first commit

---
+++ src/components/Teachers/Teachers.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Teachers.css';
+
+function Teachers(){
+	return(
+		<div className="outerContainer">
+			<div className="container">
+				<h1>Teachers</h1>
+			</div>
+		</div>
+		)
+}
+
+export default Teachers;
